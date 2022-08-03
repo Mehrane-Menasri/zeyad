8e649e46490eca9623c63ab2ba543af8b9a7d722
Mehrane-Menasri <mehranemen@gmail.com>
2022-08-04 01:53:55 +0200

    Hello Zeyad

---
+++ js/script.js
@@ -0,0 +1,210 @@
+const translations = {
+    en: {
+        home:           'Home',
+        about:          'About Us',
+        skills:         'Skills',
+        services:       'Services',
+        portfolio:      'Portfolio',
+        testimonials:   'Testimonials',
+        blog:           'blog',
+        contact:        'Contact Us',
+        arabic:         'Arabic',
+        english:        'English',
+        login:          'Login',
+        // hero section
+        herobtn1:       'Get Started',
+        herobtn2:       'Contact Us',
+        heroh1:         'Create Your Website Now',
+        herop:          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        // about section
+        abouth6:        'A little about us',
+        abouth2:        'About Us',
+        aboutp:         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        email:          'Email',
+        phone:          'Phone',
+        address:        'Adress',
+        aboutaddress:   'Country, City, 23500',
+        // skills section
+        skillsh6:       'What skills and techniques do I use?',
+        skillsh2:        'Skills',
+        skillsp:        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        design:         'Design',
+        development:    'Development',
+        html:           'A markup language used to create and design web pages and websites',
+        css:            'A markup language used to create and design web pages and websites',
+        js:             'A high-level programming language used primarily in web browsers to create more interactive pages',
+        bootstrap:      'An open source framework that helps design websites and applications',
+        php:            'A scripting language primarily designed to be used for developing and programming web applications',
+        mysql:          'Relational database management system based on SQL',
+        laravel:        'An open source PHP framework for web applications.',
+        wordpress:      'An open source electronic content management system, built in PHP and MySQL databases',
+        // services section
+        servicesh6:     'What do we offer on our site?',
+        servicesh2:     'Services',
+        servicesp:      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        designh6:       'Web Design',
+        designp:        'Get an attractive and elegant design for your website, that helps you attract more visitors and gives a distinctive impression to your customers',
+        developmenth6:  'Web Development',
+        developmentp:   'We develop dynamic websites that meet the needs of companies, improve the performance of corporate activities and provide innovative and effective solutions.',
+        wordpressh6:    'Wordpress',
+        wordpressp:     'Website design and development using WordPress online. 100% self-managed websites. Integrated management system.',
+        // portfolio section
+        portfolioh6:     'A review of some of the work we\'ve done',
+        portfolioh2:     'Portfolio',
+        portfoliop:      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        project1:        'Project No 01',
+        project2:        'Project No 02',
+        project3:        'Project No 03',
+        project4:        'Project No 04',
+        project5:        'Project No 05',
+        project6:        'Project No 06',
+        project7:        'Project No 07',
+        project8:        'Project No 08',
+        // price section
+        priceh6:         'Pricing for our special services',
+        priceh2:         'Prices',
+        pricep:          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        buy:             'Buy Now',
+        landing:         'Landing Page',
+        ecommerce:       'Ecommerce',
+        // testimonials section
+        testimonialsh6:         'Some opinions of our customers',
+        testimonialsh2:         'Testimonials',
+        testimonialsp:          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        someone:                'Someone',
+        job:                    'Web Designer',
+        // blog section
+        blogh6:     'Follow our daily articles about programming new',
+        blogh2:     'Blog',
+        blogp:      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        title:      'Article Title',
+        more:       'Read More',
+        // contact section
+        contacth6:     'For any inquiries or service, please feel free to contact',
+        contacth2:     'Contact',
+        contactp:      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        submit:        'Submit',
+        // contact section
+        contacth6:     'For any inquiries or service, please feel free to contact',
+        contacth2:     'Contact',
+        contactp:      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        submit:        'Submit',
+        group:         'Our Group',
+        // modal
+        login:        'Login',
+        close:        'Close',
+        password:     'Password',
+    }, 
+    ar: {
+        home:           'الرئيسية',
+        about:          'من نحن',
+        skills:         'المهارات',
+        services:       'الخدمات',
+        portfolio:      'معرض الأعمال',
+        testimonials:   'قالوا عنا',
+        blog:           'المدونة',
+        contact:        'اتصل بنا',
+        arabic:         'العربية',
+        english:        'الإنجليزية',
+        login:          'الدخول',
+        // hero section
+        herobtn1:       'أنشئ موقعك',
+        herobtn2:       'اتصل الآن',
+        heroh1:         'أنشئ موقعك الآن',
+        herop:          'نعمل على تقديم خدمة تصميم و برمجة المواقع بأسلوب فريد و سهل الوصول لجميع العملاء المحتملين ، نبني موقعك بكل التفاصيل التي تحتاجها و نجعل منه صرحاً زاخراً بهويتك',
+        // about section
+        abouth6:        'نبذة مختصرة عنا',
+        abouth2:        'من نحن؟',
+        aboutp:         'نعمل على تقديم خدمة تصميم و برمجة المواقع بأسلوب فريد و سهل الوصول لجميع العملاء المحتملين ، نبني موقعك بكل التفاصيل التي تحتاجها و نجعل منه صرحاً زاخراً بهويتك',
+        email:          'البريد الالكتروني',
+        phone:          'الهاتف',
+        address:        'النوان',
+        aboutaddress:   'البلد، الولاية، 2365100',
+        // skills section
+        skillsh6:       'What skills and techniques do I use?',
+        skillsh2:        'Skills',
+        skillsp:        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, possimus. Ullam dolor eum possimus nisi inventore sunt porro,',
+        design:         'Design',
+        development:    'Development',
+        html:           'لغة ترميز تستخدم في إنشاء وتصميم صفحات ومواقع الويب',
+        css:            'لغة ترميز تستخدم في إنشاء وتصميم صفحات ومواقع الويب',
+        js:             'لغة برمجة عالية المستوى تستخدم أساساً في متصفحات الويب لإنشاء صفحات أكثر تفاعلية',
+        bootstrap:      'إطار عمل مفتوح المصدر يُساعد على تصميم مواقع الويب وتطبيقاته',
+        php:            'لغة برمجة نصية صممت أساسًا من أجل استخدامها لتطوير وبرمجة تطبيقات الويب',
+        mysql:          'نظام إدارة قواعد البيانات علائقي يعتمد التعامل معه على لغة إس كيو إل',
+        laravel:        'إطار عمل لتطبيقات الويب مفتوح المصدر بلغة PHP.',
+        wordpress:      'نظام إدارة محتوى إلكتروني مفتوح المصدر، مبنيّ بلغة بي إتش بي وقواعد بيانات ماي إس كيو إل',
+        // services section
+        servicesh6:     'ماذ نقدم في موقعنا؟',
+        servicesh2:     'الخدمات',
+        servicesp:      'نعمل على تقديم خدمة تصميم و برمجة المواقع بأسلوب فريد و سهل الوصول لجميع العملاء المحتملين ، نبني موقعك بكل التفاصيل التي تحتاجها و نجعل منه صرحاً زاخراً بهويتك',
+        designh6:       'تصميم المواقع',
+        designp:       'احصل على تصميم جذاب وأنيق لموقعك الالكتروني ، يساعدك على جلب المزيد من الزوار ويعطي انطباع مميز لعملائك        ',
+        developmenth6:  'برمجة المواقع',
+        developmentp:   'نقوم بتطوير مواقع ديناميكية تلبي احتياجات الشركات كما نقوم بتحسين أداء أنشطة الشركات وتقديم حلول مبتكرة وفعالة.',
+        wordpressh6:    'الووردبرس',
+        wordpressp:     'تصميم وتطوير المواقع باستخدام ووردبرس عبر الإنترنت.100 ٪ مواقع ذاتية الإدارة. نظام إدارة متكامل.',
+        // portfolio section
+        portfolioh6:     'استعراض لبعض الأعمال التي قمنا بها',
+        portfolioh2:     'الخدمات',
+        portfoliop:      'نعمل على تقديم خدمة تصميم و برمجة المواقع بأسلوب فريد و سهل الوصول لجميع العملاء المحتملين ، نبني موقعك بكل التفاصيل التي تحتاجها و نجعل منه صرحاً زاخراً بهويتك',
+        project1:        'مشروع رقم 01',
+        project2:        'مشروع رقم 02',
+        project3:        'مشروع رقم 03',
+        project4:        'مشروع رقم 04',
+        project5:        'مشروع رقم 05',
+        project6:        'مشروع رقم 06',
+        project7:        'مشروع رقم 07',
+        project8:        'مشروع رقم 08',
+        // price section
+        priceh6:     'أسعار خدماتنا المميزة',
+        priceh2:     'الاسعار',
+        pricep:      'نعمل على تقديم خدمة تصميم و برمجة المواقع بأسلوب فريد و سهل الوصول لجميع العملاء المحتملين ، نبني موقعك بكل التفاصيل التي تحتاجها و نجعل منه صرحاً زاخراً بهويتك',
+        buy:             'اشتر الآن',
+        landing:         'صفحة هبوط',
+        ecommerce:       'متجر الكتروني',
+        // testimonials section
+        testimonialsh6:     'بعض آراء زبائننا',
+        testimonialsh2:     'قالوا عنا',
+        testimonialsp:      'نعمل على تقديم خدمة تصميم و برمجة المواقع بأسلوب فريد و سهل الوصول لجميع العملاء المحتملين ، نبني موقعك بكل التفاصيل التي تحتاجها و نجعل منه صرحاً زاخراً بهويتك',
+        someone:             'فلان بن فلان',
+        job:         'مصمم مواقع',
+        // blog section
+        blogh6:     'تابع مقالاتنا اليومية حول جديد البرمجة',
+        blogh2:     'المدونة',
+        blogp:      'نعمل على تقديم خدمة تصميم و برمجة المواقع بأسلوب فريد و سهل الوصول لجميع العملاء المحتملين ، نبني موقعك بكل التفاصيل التي تحتاجها و نجعل منه صرحاً زاخراً بهويتك',
+        title:             'عنوان المقال',
+        more:             'اقرأ المزيد',
+        // contact section
+        contacth6:     'لأي استفسار أو خدمة لا تتردد في الاتصال',
+        contacth2:     'اتصل بنا',
+        contactp:      'نعمل على تقديم خدمة تصميم و برمجة المواقع بأسلوب فريد و سهل الوصول لجميع العملاء المحتملين ، نبني موقعك بكل التفاصيل التي تحتاجها و نجعل منه صرحاً زاخراً بهويتك',
+        submit:             'ارسال',
+        group:         'مجموعتنا',
+        // modal
+        login:        'الدخول',
+        close:        'إغلاق',
+        password:     'كلمة المرور',
+    }
+}
+
+const languageSelector = document.querySelector("select")
+
+languageSelector.addEventListener("change", (event) => {
+    setLanguage(event.target.value)
+    localStorage.setItem('lang', event.target.value)
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const language = localStorage.getItem("lang")
+})
+
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll("[data-lang]");
+    elements.forEach((element) => {
+        const translationKey = element.getAttribute("data-lang");
+        element.textContent = translations[language][translationKey];
+    });
+    document.dir = language === "ar" ? "rtl" : "ltr";
+}
+
